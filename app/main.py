533bec050aa1f6d6d07a654e12c8cee1a140cfe5
Salaarkhaa9 <kowalskikh92@gmail.com>
2026-06-18 00:16:17 +0500

    second push

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"status": "ok", "message": "DevOps pipeline is live!"})
+    return jsonify({"status": "ok", "message": "DevOps pipeline is live! v1.0"})
 
 @app.route("/health")
 def health():
